4d7a1d773cccb791445bc4aadcda6b9bd8ec1eb1
wangmeng8 <wangmeng8@xiaomi.com>
2019-01-18 11:23:04 +0800

    增加抽象类和接口

---
+++ src/main/java/com/demo/test/AbstractTest.java
@@ -0,0 +1,17 @@
+package com.demo.test;
+
+/**
+ * @author wangmeng
+ * @date 19/1/18
+ */
+public abstract class AbstractTest {
+    public abstract void test1();
+    public abstract void test2();
+
+
+    public void test3(){
+
+    }
+
+
+}
---
+++ src/main/java/com/demo/test/InterFaceTest.java
@@ -0,0 +1,11 @@
+package com.demo.test;
+
+/**
+ * @author wangmeng
+ * @date 19/1/18
+ */
+public interface InterFaceTest {
+    void inter1();
+    void inter2();
+
+}
